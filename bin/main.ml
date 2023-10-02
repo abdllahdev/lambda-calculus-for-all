a03ec2c681e8ebdd971be707366546ad0ad733da
Abdullah Elsayed <11673834+abdllahdev@users.noreply.github.com>
2023-10-02 23:42:57 +0100

    feat: implement booleans

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,7 @@
-let () = print_endline "Lambda calculus tutorial"
+open Simply_lambda
+
+let () =
+  let and_results = Booleans.test_op ~op:AND in
+  let or_results = Booleans.test_op ~op:OR in
+  let not_results = Booleans.test_op ~op:NOT in
+  Printf.printf "%s\n\n%s\n\n%s\n" and_results or_results not_results
---
+++ lib/booleans.ml
@@ -0,0 +1,48 @@
+open Core
+
+type 'a boolean = 'a -> 'a -> 'a
+
+let true' : 'a boolean = fun b1 _b2 -> b1
+let false' : 'a boolean = fun _b1 b2 -> b2
+
+let ( && ) (x : 'a boolean) (y : 'a boolean) : 'a boolean =
+ fun t f -> x (y t f) f
+
+let ( || ) (x : 'a boolean) (y : 'a boolean) : 'a boolean =
+ fun t f -> x t (y t f)
+
+let ( ! ) (x : 'a boolean) : 'a boolean = fun t f -> x f t
+
+type op = AND | OR | NOT
+
+let string_of_booleans (bs : 'a boolean list) =
+  List.map ~f:(fun b -> b "true" "false") bs |> String.concat ~sep:", "
+
+let test_op ~op =
+  match op with
+  | AND ->
+      List.map
+        ~f:(fun (x, y) ->
+          let res = x && y in
+          Fmt.str "(%s) -> %s"
+            (string_of_booleans [ x; y ])
+            (string_of_booleans [ res ]))
+        [ (false', false'); (false', true'); (true', false'); (true', true') ]
+      |> String.concat ~sep:"\n" |> Fmt.str "AND Results:\n%s"
+  | OR ->
+      List.map
+        ~f:(fun (x, y) ->
+          let res = x || y in
+          Fmt.str "(%s) -> %s"
+            (string_of_booleans [ x; y ])
+            (string_of_booleans [ res ]))
+        [ (false', false'); (false', true'); (true', false'); (true', true') ]
+      |> String.concat ~sep:"\n" |> Fmt.str "OR Results:\n%s"
+  | NOT ->
+      List.map
+        ~f:(fun x ->
+          let res = !x in
+          Fmt.str "%s -> %s" (string_of_booleans [ x ])
+            (string_of_booleans [ res ]))
+        [ false'; true' ]
+      |> String.concat ~sep:"\n" |> Fmt.str "NOT Results:\n%s"
